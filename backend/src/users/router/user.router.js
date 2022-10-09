7952a8352200cc60c4bb8db72cd78cb4cc8e9e01
Jaimik Vandra <34745310+Jaimik907@users.noreply.github.com>
2022-10-09 22:58:07 +0530

    add validation middleware for add/edit user (#4)

--- backend/src/users/router/user.router.js
+++ backend/src/users/router/user.router.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const emailValidator = require('email-validator');
 const userController = require('../controller/user.controller');
 
 const router = express.Router();
 
 router.get('/', userController.getAllUser);
-router.post('/add', userController.addUser);
-router.put('/edit/:id', userController.editUser);
+
+function checkEmail(req, res, next) {
+  if (!emailValidator.validate(req.body.email)) {
+    res.status(400).json({ message: 'Invalid Email format' });
+    return;
+  }
+  next();
+}
+router.post('/add', checkEmail, userController.addUser);
+router.put('/edit/:id', checkEmail, userController.editUser);
 router.delete('/delete/:id', userController.deleteUser);
 router.get('/:id', userController.getUser);
 
