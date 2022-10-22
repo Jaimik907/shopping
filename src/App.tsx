25b120d301f7b2e199ac6f8f7a1aa036e2806dd3
Jaimik Vandra <34745310+Jaimik907@users.noreply.github.com>
2022-10-23 00:33:39 +0530

    Remove cart module (#9)
    * remove all cart things
    
    * remove cart from store

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
-import { useAuth } from './context/authProvider';
-import { calculateTotals } from './features/cart/cartSlice';
-import AppRouting from './routes/routing';
-import { RootState } from './store/store';
 
 function App() {
-  const { cartItems } = useSelector((state: RootState) => state.cart);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(calculateTotals());
-  }, [cartItems, dispatch]);
-
-  return <AppRouting />;
+  return <h1>App component</h1>;
 }
 
 export default App;
