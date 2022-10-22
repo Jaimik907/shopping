bc7bede59c02946c3adf06b0d4c00782f4e24e5b
Jaimik Vandra <jaimikvandra907@gmail.com>
2022-10-23 00:32:47 +0530

    remove cart from store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,18 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
-import cartSlice from '../features/cart/cartSlice';
-import userSlice from '../features/user/userSlice';
-import cartSaga from '../sagas/cart';
 
 const saga = createSagaMiddleware();
 export const store = configureStore({
   reducer: {
-    cart: cartSlice,
-    user: userSlice,
+
   },
   middleware: [saga],
 });
-saga.run(cartSaga);
+// saga.run();
 
 export type RootState = ReturnType<typeof store.getState>;
 export type RootDispatch = typeof store.dispatch;
